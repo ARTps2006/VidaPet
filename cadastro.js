b5532b465b8daab3d3c67d531346b35ac59519d7
ARTps2006 <arthurpessoadasilva192@gmail.com>
2026-03-09 20:01:56 -0300

    fazendo cadastro

--- cadastro.js
+++ cadastro.js
@@ -1,33 +1,43 @@
-const botao = document.querySelector("button");
+function cadastrar() {
+
 const nome = document.getElementById("nome");
+const usuario = document.getElementById("usuario");
 const email = document.getElementById("email");
+const telefone = document.getElementById("telefone");
 const senha = document.getElementById("senha");
-const mensagem = document.getElementById("mensagemErro");
+const confirmarSenha = document.getElementById("confirmarSenha");
 
-botao.addEventListener("click", function () {
+const mensagemErro = document.getElementById("mensagemErro");
+const mensagemSucesso = document.getElementById("mensagemSucesso");
 
-    if (nome.value === "" || email.value === "" || senha.value === "") {
-        mensagem.style.color = "red";
-        mensagem.textContent = "Preencha todos os campos!";
-        mensagem.classList.add("mostrar");
-        return;
-    }
+mensagemErro.classList.remove("mostrar");
+mensagemSucesso.classList.remove("mostrar");
 
-    if (!email.value.includes("@") || !email.value.includes(".")) {
-        mensagem.style.color = "red";
-        mensagem.textContent = "Digite um email válido com @gmail.com";
-        mensagem.classList.add("mostrar");
-        return;
-    }
+if (nome.value === "" || usuario.value === "" || email.value === "" || telefone.value === "" || senha.value === "" || confirmarSenha.value === "") {
 
-    if (senha.value.length < 6) {
-        mensagem.style.color = "red";
-        mensagem.textContent = "A senha deve ter pelo menos 6 caracteres!";
-        mensagem.classList.add("mostrar");
-        return;
-    }
+mensagemErro.textContent = "Preencha todos os campos!";
+mensagemErro.classList.add("mostrar");
+return;
 
-    mensagem.style.color = "green";
-    mensagem.textContent = "Cadastro realizado com sucesso!";
-    mensagem.classList.add("mostrar");
-});
+}
+
+if (senha.value !== confirmarSenha.value) {
+
+mensagemErro.textContent = "As senhas não são iguais!";
+mensagemErro.classList.add("mostrar");
+return;
+
+}
+
+if (senha.value.length < 6) {
+
+mensagemErro.textContent = "A senha deve ter pelo menos 6 caracteres!";
+mensagemErro.classList.add("mostrar");
+return;
+
+}
+
+mensagemSucesso.textContent = "Cadastro realizado com sucesso!";
+mensagemSucesso.classList.add("mostrar");
+
+}
