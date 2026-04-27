ef7851aaf112b4be69acacd0e1d3782eeb29bfaf
ARTps2006 <arthurpessoadasilva192@gmail.com>
2026-04-27 09:28:18 -0300

    atualizei as páginas

--- home.js
+++ home.js
@@ -5,3 +5,41 @@ function perfil() {
 function sair() {
     window.location.href = "login.html"
 }
+
+let indice = 0;
+
+const depoimentos = document.querySelectorAll(".depoimento");
+
+function mostrarDepoimento() {
+
+    depoimentos.forEach((d) => {
+        d.classList.remove("ativo");
+    });
+
+    depoimentos[indice].classList.add("ativo");
+
+}
+
+function proximo() {
+
+    indice++;
+
+    if (indice >= depoimentos.length) {
+        indice = 0;
+    }
+
+    mostrarDepoimento();
+
+}
+
+function anterior() {
+
+    indice--;
+
+    if (indice < 0) {
+        indice = depoimentos.length - 1;
+    }
+
+    mostrarDepoimento();
+
+}
