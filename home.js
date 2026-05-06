96f32db7631058f28178703477ba88f7df3610ec
Arthur Pessoa da Silva <arthurpessoadasilva192@gmail.com>
2026-05-06 07:52:35 -0300

    .

--- home.js
+++ home.js
@@ -1,27 +1,23 @@
 function perfil() {
-    window.location.href = "perfil.html"
+    window.location.href = "perfil.html";
 }
 
 function sair() {
-    window.location.href = "login.html"
+    window.location.href = "login.html";
 }
 
 let indice = 0;
-
 const depoimentos = document.querySelectorAll(".depoimento");
 
 function mostrarDepoimento() {
-
     depoimentos.forEach((d) => {
         d.classList.remove("ativo");
     });
 
     depoimentos[indice].classList.add("ativo");
-
 }
 
 function proximo() {
-
     indice++;
 
     if (indice >= depoimentos.length) {
@@ -29,11 +25,9 @@ function proximo() {
     }
 
     mostrarDepoimento();
-
 }
 
 function anterior() {
-
     indice--;
 
     if (indice < 0) {
@@ -41,43 +35,41 @@ function anterior() {
     }
 
     mostrarDepoimento();
+}
 
+//Comentários//
 
-    const form = document.getElementById("form-comentario");
-    const input = document.getElementById("input-comentario");
-    const lista = document.getElementById("lista-comentarios");
+const form = document.getElementById("form-comentario");
+const input = document.getElementById("input-comentario");
+const lista = document.getElementById("lista-comentarios");
 
-    // carregar comentários salvos
-    let comentarios = JSON.parse(localStorage.getItem("comentarios")) || [];
+let comentarios = JSON.parse(localStorage.getItem("comentarios")) || [];
 
-    function renderizarComentarios() {
-        lista.innerHTML = "";
+function renderizarComentarios() {
+    lista.innerHTML = "";
 
-        comentarios.forEach((comentario) => {
-            const div = document.createElement("div");
-            div.classList.add("comentario");
-            div.textContent = comentario;
-            lista.appendChild(div);
-        });
-    }
+    comentarios.forEach((comentario) => {
+        const div = document.createElement("div");
+        div.classList.add("comentario");
+        div.textContent = comentario;
+        lista.appendChild(div);
+    });
+}
+
+renderizarComentarios();
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const texto = input.value.trim();
+
+    if (texto === "") return;
+
+    comentarios.push(texto);
+
+    localStorage.setItem("comentarios", JSON.stringify(comentarios));
+
+    input.value = "";
 
     renderizarComentarios();
-
-    // adicionar novo comentário
-    form.addEventListener("submit", (e) => {
-        e.preventDefault();
-
-        const texto = input.value.trim();
-
-        if (texto === "") return;
-
-        comentarios.push(texto);
-
-        localStorage.setItem("comentarios", JSON.stringify(comentarios));
-
-        input.value = "";
-
-        renderizarComentarios();
-    });
-
-}
+});
